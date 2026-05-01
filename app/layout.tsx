fdfc62f8341c6a12b3a1cc1c287d7f832a05180c
James Vongampai <jaythomasv29@gmail.com>
2026-04-30 22:56:00 -0700

    add routes and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+
+import { cn } from "@/lib/utils";
+import { Navbar } from "@/components/web/navbar";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +31,28 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans",
+        inter.variable,
+      )}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8">
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ components/web/navbar.tsx
@@ -0,0 +1,42 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { ThemeToggle } from "./theme-toggle";
+
+export function Navbar() {
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+      <div className="flex items center gap-8">
+        {/* Nav Title */}
+        <Link href="/">
+          <h1 className="text-3xl font-bold">
+            Next<span className="text-blue-500">Pro</span>
+          </h1>
+        </Link>
+        {/* Nav Links */}
+        <div className="flex items-center gap-2">
+          <Link className={buttonVariants({ variant: "ghost" })} href="/">
+            Home
+          </Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/blog">
+            Blog
+          </Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/create">
+            Create
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-2">
+        <Link className={buttonVariants()} href="/auth/sign-up">
+          Sign up
+        </Link>
+        <Link
+          className={buttonVariants({ variant: "outline" })}
+          href="/auth/login"
+        >
+          Login
+        </Link>
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
+}
